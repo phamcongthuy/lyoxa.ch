44575ab246f675ced937bbaf82591e0cb82d4473
rspt <julien@rspt.ch>
2017-04-07 11:27:23 +0200

    Update navbar toggle references

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
   <nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <nuxt-link to="/" class="navbar-brand">
       <img src="~assets/logo.svg" width="73" height="30" alt="Lyoxa's logo">
     </nuxt-link>
 
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <div class="collapse navbar-collapse" id="navbarToggler">
       <ul class="navbar-nav ml-auto mt-2 mt-md-0">
         <li class="nav-item">
           <nuxt-link to="/" class="nav-link" exact>Accueil</nuxt-link>
