1fcbd58b3d9193ee73882077e112f9b35bc26b2e
rspt <julien@rspt.ch>
2017-04-12 21:21:14 +0200

    Wrap pages in container

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="container">
     <h1>Home</h1>
   </section>
 </template>
--- pages/partenaires/index.vue
+++ pages/partenaires/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="container">
     <h1>Partenaires</h1>
   </section>
 </template>
