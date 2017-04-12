cfe1fb5bce3ad2b9bb58ab8f95cc5fc8fb8783fb
rspt <julien@rspt.ch>
2017-04-12 21:13:49 +0200

    Update Discotime

--- pages/discotime/index.vue
+++ pages/discotime/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/imYJpr09IgQ?rel=0&autoplay=1" frameborder="0" allowfullscreen autoplay></iframe>
+  <div class="container">
+    <h1>Disco Time 🔥🎉🦄</h1>
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/imYJpr09IgQ?rel=0&autoplay=1" frameborder="0" allowfullscreen autoplay></iframe>
+    </div>
   </div>
 </template>
