0f882b51da017d348fab54dcaabb7de430950402
rspt <julien@rspt.ch>
2017-04-13 11:07:08 +0200

    Create the Team page

---
+++ components/Team.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="col-6 col-md-12">
+      <img :src="photo" :alt="name" class="w-100 mb-3">
+    </div>
+    <div class="col-6 col-md-12">
+      <p class="font-weight-bold mb-0 text-uppercase">{{ name }}</p>
+      <p>{{ title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'name',
+    'title',
+    'photo'
+  ]
+}
+</script>
--- pages/equipe/index.vue
+++ pages/equipe/index.vue
@@ -1,5 +1,50 @@
 <template>
-  <section>
+  <section class="container">
     <h1>Équipe</h1>
+    <div class="multicolumnparagraph">
+      <p>
+        Notre équipe est principalement composée de jeunes universitaires, étudiants en <i>Droit et Relations Internationales</i>. Depuis nos années au collège, nous faisons partie de plusieurs associations genevoises estudiantines. Par notre engagement, nous avons eu l'occasion de toucher à tous les domaines liés à l'activité associative. Curieux et avides d'expérience, nous nous sommes intéressés à la large scène genevoise associative et avons forgé un grand réseau de connaissances. De par les postes que nous avons occupés au sein de nos associations, nous avons acquis et maîtrisé les compétences administratives requises pour leur bon fonctionnement.
+      </p>
+      <p>
+        De plus, grâce à cette expérience, nous avons reçu de nombreuses demandes individuelles de soutien et d’informations de notre réseau de connaissances. Nous avons dû intervenir dans plusieurs événements, conseiller des associations dans leur fonctionnement et financement, nous renseigner auprès des autorités des nouvelles attentes, négocier auprès d’entreprises et de propriétaires de salle ainsi que de mettre en contact les associations avec nos précédents partenaires. Ces actions ont été faites à titre individuel et ce, de manière fréquente.
+      </p>
+      <p>
+        Actuellement l’association est composée des trois membres fondateurs présentés ci-dessous. Nous travaillons toutefois dur pour agrandir cette équipe en recrutant parmi les personnes les plus motivées et engagées possibles.
+      </p>
+    </div>
+    <h2 class="mt-5">Membres fondateurs</h2>
+    <div class="row">
+      <div class="col-md-4">
+        <team
+          name="Célia Sepe"
+          title="Secrétaire Générale"
+          photo="/img/equipe/sepe-celia.png">
+        </team>
+      </div>
+      <div class="col-md-4">
+        <team
+          name="Stéphanie Catterson"
+          title="Trésorière"
+          photo="https://unsplash.it/200/300?image=1027">
+        </team>
+      </div>
+      <div class="col-md-4">
+        <team
+          name="Nicolas Tamayo Lopez"
+          title="Président"
+          photo="/img/equipe/tamayo-lopez-nicolas.png">
+        </team>
+      </div>
+    </div>
   </section>
 </template>
+
+<script>
+import Team from '~components/Team.vue'
+
+export default {
+  components: {
+    Team
+  }
+}
+</script>
