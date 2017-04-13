9f8ecfc2f0e5592cfbb7bdfd59f289f95d46bf6f
rspt <julien@rspt.ch>
2017-04-13 16:34:22 +0200

    Add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,10 +12,16 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '32x16' },
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#cb0000' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik' }
     ]
   },
