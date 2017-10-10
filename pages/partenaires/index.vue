ddd796ddda9fc15b1ec1e46c17a7a40e74652b54
rspt <julien@rspt.ch>
2017-10-10 17:09:52 +0200

    Remove unused code

--- pages/partenaires/index.vue
+++ pages/partenaires/index.vue
@@ -36,35 +36,6 @@
         </partenaire>
       </div>
     </div>
-
-    <!-- <div class="card-deck">
-      <partenaire
-        title="laruche"
-        description="Présence digitale, développement d'applications et de sites web."
-        buttonLink="https://laruche.io"
-        logo="laruche.svg">
-      </partenaire>
-      <partenaire
-        title="Hypersquad"
-        description="Studio de graphisme pluridisciplinaire et nouveaux médias."
-        buttonLink="https://hypersquad.ch"
-        logo="hypersquad.svg">
-      </partenaire>
-      <partenaire
-        title="Bassment!"
-        description="Association promouvant la culture et son accessibilité."
-        buttonLink="https://bassment.ch"
-        logo="bassment.svg">
-      </partenaire>
-    </div>
-    <div class="card-deck">
-      <partenaire
-        title="DJ-R"
-        description="Location de sonorisation et d'éclairage pour votre événement."
-        buttonLink="http://www.dj-r.ch/"
-        logo="dj-r.jpg">
-      </partenaire>
-    </div> -->
   </section>
 </template>
 
