c23029feeeb28cc0b8d236f6d1b6726742d25ffc
rspt <julien@rspt.ch>
2019-01-20 14:20:20 +0100

    Default lyaout and basic homepage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <nav class="navbar navbar-light bg-white">
+      <div class="container">
+        <a
+          href="/"
+          class="navbar-brand">
+          <img
+            src="~/assets/img/lyoxa-logo.svg"
+            alt="Logo de Lyoxa"
+            height="30px"
+            width="73px">
+        </a>
+      </div>
+    </nav>
     <nuxt/>
   </div>
 </template>
---
+++ pages/lyoxa/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Lyoxa
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
